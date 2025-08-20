88a0b7e399a1cc47fcc3d0dcbaeb73948a3de9b2
Phalgun <phalgun.kulkarni@aon.com>
2025-08-20 09:47:42 -0400

    Update support.py

--- src/qelp/support.py
+++ src/qelp/support.py
@@ -269,7 +269,7 @@ class Configure:
         parser.add_argument(
             "input_dir",
             type=self.path,
-            help="The input directory containing .zip, .tar.gz, .tgz, or .tar files",
+            help="The input directory containing .zip, .tar, .gz, or .tgz files",
         )
         parser.add_argument(
             "output_dir",
